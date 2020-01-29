699581489d5838db673e0e4999cbb72edf6164d1
sasha walker <sashawalker31@gmail.com>
2020-01-29 15:31:52 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+sentence1=("Hello there, and how are you?")
+sentence2=("Hi again, just making sure it's reversed!")
+
+def reverse_each_word()
+  puts sentence1.reverse
+end
+  puts sentence2.reverse
+end
